cf71b8c920fef89fa8536d2fb78f60935ddacdfe
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-06-02 12:48:43 -0500

    deployin in heroku

--- src/routes/routeProducts.js
+++ src/routes/routeProducts.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const routerProducts = express.Router()
 
-const {getProducts, getProductById, getProductByCategoryId, createProduct, updateProduct, deleteProduct} = require('../controllers/ProductsController')
+const {getProducts, getProductById, getProductByCategoryId, createProduct, updateProduct, deleteProduct} = require('../controllers/productsController')
 
 routerProducts.route('/').get(getProducts).post(createProduct)
 routerProducts.route('/:id').get(getProductById).put(updateProduct).delete(deleteProduct)
