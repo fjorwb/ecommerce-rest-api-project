2a49e0b531a64c828b4d5a909bc8da9218290ebc
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-13 15:04:57 -0500

    tunning passport

--- passportConfig.js
+++ passportConfig.js
@@ -9,24 +9,49 @@ function initialize (passport) {
     const statement = 'SELECT * FROM users WHERE email = $1'
     const values = [email]
 
-    const result = await db.query(statement, values)
-      .then(res => {
-        if (res.rows.length === 0) {
-          return done(null, false, { message: 'Incorrect email.' })
-        } else {
-          const user = res.rows[0]
-          if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, false, { message: 'Incorrect password.' })
-          } else {
+    const results = await db.query(statement, values)
+    console.log(results)
+
+    if (results.length === 0) {
+      return done(null, false, { message: 'Incorrect email.' })
+    } else {
+      if (results.length > 0) {
+        const user = results[0]
+
+        bcrypt.compare(password, user.password, (error, isMatch) => {
+          if (error) {
+            throw error
+          }
+          if (isMatch) {
             return done(null, user)
-          } // end else
-        } // end else
-      }).catch(err => {
-        console.log(err)
-        return done(err)
-      }).finally(() => {
-        db.end()
-      })
+          } else {
+            // password is incorrect
+            return done(null, false, { message: 'Password is incorrect' })
+          }
+        })
+      } else {
+        // No user
+        return done(null, false, {
+          message: 'No user with that email. please register'
+        })
+      }
+    }
+
+    // .then(res => {
+    //   if (res.rows.length === 0) {
+    //     return done(null, false, { message: 'Incorrect email.' })
+    //   } else {
+    //     const user = res.rows[0]
+    //     if (!bcrypt.compareSync(password, user.password)) {
+    //       return done(null, false, { message: 'Incorrect password.' })
+    //     } else {
+    //       return done(null, user)
+    //     } // end else
+    //   } // end else
+    // }).catch(err => {
+    //   console.log(err)
+    //   return done(err)
+    // })
 
     // pool.query(
     //   'SELECT * FROM users WHERE email = $1',
