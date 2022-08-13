eb357c0917f2dc484532aa2640cca2a5d46c62e9
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-13 15:37:56 -0500

    tunning

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -14,9 +14,9 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
-  // max: 20,
-  // ssl
+  port: process.env.PGPORT,
+  max: 20,
+  ssl
 })
 
 const connection = ({
