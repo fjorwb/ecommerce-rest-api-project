f076ee591766ce8ad0aee3549ac2b1af36449855
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-13 14:29:24 -0500

    tunning

--- passportConfig.js
+++ passportConfig.js
@@ -1,40 +1,64 @@
 const LocalStrategy = require('passport-local').Strategy
-const { pool } = require('./src/dbConfig')
+
+const { pool, db } = require('./src/dbConfig')
+
 const bcrypt = require('bcrypt')
 
 function initialize (passport) {
-  const authenticateUser = (email, password, done) => {
-    pool.query(
-      'SELECT * FROM users WHERE email = $1',
-      [email],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        console.log(results.rows[0])
+  const authenticateUser = async (email, password, done) => {
+    const statement = 'SELECT * FROM users WHERE email = $1'
+    const values = [email]
 
-        if (results.rows.length > 0) {
-          const user = results.rows[0]
-
-          bcrypt.compare(password, user.password, (error, isMatch) => {
-            if (error) {
-              throw error
-            }
-            if (isMatch) {
-              return done(null, user)
-            } else {
-              // password is incorrect
-              return done(null, false, { message: 'Password is incorrect' })
-            }
-          })
+    const result = await db.query(statement, values)
+      .then(res => {
+        if (res.rows.length === 0) {
+          return done(null, false, { message: 'Incorrect email.' })
         } else {
-          // No user
-          return done(null, false, {
-            message: 'No user with that email. please register'
-          })
-        }
-      }
-    )
+          const user = res.rows[0]
+          if (!bcrypt.compareSync(password, user.password)) {
+            return done(null, false, { message: 'Incorrect password.' })
+          } else {
+            return done(null, user)
+          } // end else
+        } // end else
+      }).catch(err => {
+        console.log(err)
+        return done(err)
+      }).finally(() => {
+        db.end()
+      })
+
+    // pool.query(
+    //   'SELECT * FROM users WHERE email = $1',
+    //   [email],
+    //   (error, results) => {
+    //     if (error) {
+    //       throw error
+    //     }
+    //     console.log(results.rows[0])
+
+    //     if (results.rows.length > 0) {
+    //       const user = results.rows[0]
+
+    //       bcrypt.compare(password, user.password, (error, isMatch) => {
+    //         if (error) {
+    //           throw error
+    //         }
+    //         if (isMatch) {
+    //           return done(null, user)
+    //         } else {
+    //           // password is incorrect
+    //           return done(null, false, { message: 'Password is incorrect' })
+    //         }
+    //       })
+    //     } else {
+    //       // No user
+    //       return done(null, false, {
+    //         message: 'No user with that email. please register'
+    //       })
+    //     }
+    //   }
+    // )
   }
 
   passport.use(
