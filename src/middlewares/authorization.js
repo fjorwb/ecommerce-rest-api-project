38d42066bd9cbf7dfc531fc7dc679a3a3aad5adc
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-15 16:57:44 -0500

    finish authorization middleware

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,15 +1,12 @@
 // const db = require('./dbConfig')
 
 const authorization = (permissions) => {
-  console.log('permissions:---' + permissions)
   return (req, res, next) => {
-    console.log('req.user.role:....' + req.user.role)
-    permissions.includes((req.user.role).trim()) ? console.log('ok') : console.log('no ok')
     if (permissions.includes((req.user.role).trim())) {
       next()
     } else {
       res.status(403).json({
-        message: 'Forbidden'
+        message: 'user not authorized'
       })
     }
   }
