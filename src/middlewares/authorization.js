b8c5fd38bf58ae722e80672ce3c3c0f06073343f
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-15 16:46:13 -0500

    tunning

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,7 +1,10 @@
 // const db = require('./dbConfig')
 
 const authorization = (permissions) => {
+  console.log('permissions:---' + permissions)
   return (req, res, next) => {
+    console.log(req.user.role)
+    permissions.includes(req.user.role) ? console.log('ok') : console.log('no ok')
     if (permissions.includes(req.user.role)) {
       next()
     } else {
