a9db5dae8cf34d4e3bb5cead91a04a2955df0118
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-15 16:53:49 -0500

    tunning

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -4,8 +4,8 @@ const authorization = (permissions) => {
   console.log('permissions:---' + permissions)
   return (req, res, next) => {
     console.log('req.user.role:....' + req.user.role)
-    permissions.includes(req.user.role) ? console.log('ok') : console.log('no ok')
-    if (permissions.includes(req.user.role)) {
+    permissions.includes((req.user.role).trim()) ? console.log('ok') : console.log('no ok')
+    if (permissions.includes((req.user.role).trim())) {
       next()
     } else {
       res.status(403).json({
