49a55ad5e57e990c146cec61d90326d785eab707
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-15 15:54:50 -0500

    tunning

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,35 +1,16 @@
-const { db } = require('../dbConfig')
-
-// const ConvertObject = require('../helpers/ConvertObject')
-// const ShowUser = require('../helpers/ShowUser')
+// const db = require('./dbConfig')
 
 const authorization = (permissions) => {
-  return async (req, res, next) => {
-    const user = req.user.id
-    if (!user) {
-      res.status(400)
+  return (req, res, next) => {
+    if (req.user.role === 'admin') {
+      next()
     } else {
-      const statement = 'SELECT role FROM users WHERE id = $1'
-      const values = [user]
-
-      const result = await db.any(statement, values)
-      console.log('results:::::' + result)
-
-      const role = result[0].role
-      console.log('role::::: ' + role)
-
-      const permission = Object.values(permissions)
-      console.log('permission: ' + permission)
-
-      console.log('persmissions::::: ' + permissions[0])
-      console.log(typeof (permissions))
-      console.log('permissions includes role:::::::' + permissions.includes(role))
-
-      if (permission[0].includes(role)) {
-        console.log('allowed')
+      if (permissions.includes(req.user.role)) {
         next()
       } else {
-        res.status(401).json('not allowed')
+        res.status(403).json({
+          message: 'Forbidden'
+        })
       }
     }
   }
