4aafae5f0f94adbf4385f23851c86249d4bc0433
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-15 20:22:33 -0500

    documentation

--- src/routes/routeDocs.js
+++ src/routes/routeDocs.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const docsRouter = express.Router()
+const routerDocs = express.Router()
 const swagger = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
 
@@ -7,26 +7,27 @@ const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Portfolio / Personal Budget',
+      title: 'Portfolio / eCommerce RESTFul API',
       version: '1.0.0',
       description:
-        'Simple backend API to manage portfolio budget using an envelope budgeting method',
+        'RESTFul API for an e-commerce.',
       license: {
         name: 'MIT',
         url: 'https://choosealicense.com/licenses/mit/'
       }
     }
   },
-  apis: ['./PersonalBudget.yaml']
+  apis: ['../../swagger-ecomm.yaml']
 }
+
 const specs = swagger(swaggerOptions)
 
-docsRouter.use('/', swaggerUi.serve)
-docsRouter.get(
+routerDocs.use('/', swaggerUi.serve)
+routerDocs.get(
   '/',
   swaggerUi.setup(specs, {
     explorer: true
   })
 )
 
-module.exports = docsRouter
+module.exports = routerDocs
