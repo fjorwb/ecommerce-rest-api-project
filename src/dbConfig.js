72f411b52ce04e4b1f55d24cc53b2e38a50c8c41
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-07-07 11:30:45 -0500

    Adjust dbconfig

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -14,20 +14,20 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
+  port: process.env.PGPORT,
   max: 20,
   ssl
 })
 
-const connection = ({
+const connection = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
+  port: process.env.PGPORT,
   max: 20,
   ssl
-})
+}
 
 const db = pgp(connection)
 
