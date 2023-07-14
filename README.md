416d7bb507ec3006b978973ee4f5efdab947593c
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-07-14 11:55:21 -0500

    Adjusting to serve API

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # e-commerce RESTful API Project Plan
-![https://img.shields.io/badge/Codecademy-FFF0E5?style=for-the-badge&logo=codecademy&logoColor=303347
-](https://img.shields.io/badge/Codecademy-FFF0E5?style=for-the-badge&logo=codecademy&logoColor=303347)
-![](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
-![](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
-![](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white)
-![](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white)
-![](https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white)
+
+![codecademy badge](<https://img.shields.io/badge/Codecademy-FFF0E5?style=for-the-badge&logo=codecademy&logoColor=303347>)
+
+![nodejs bagge](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
+
+![vscode badge](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
+
+![postgresql badge](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white)
+
+![express badge](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white)
+
+![heroku badge](https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white)
 
 Table of Contents
+
 - [General Info](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#general-info--summary)
 - [Documentation](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#technologies)
 - [Technologies](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#technologies)
@@ -20,8 +26,11 @@ Table of Contents
 - [Acknowledgements](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#acknowledgements)
 - [License](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#license)
 - [Contact](https://github.com/fjorwb/ecommerce-rest-api-project/edit/master/README.md#contact)
+
 ## General Info / Summary
+
 This project is part of the Full-Stack-Engineer path and the goal of is:
+
 - Build a functioning e-commerce REST API using Express, - Node.js, and Postgres
 - Allow users to register and log in via the API
 - Allow CRUD operations on products
@@ -33,88 +42,126 @@ This project is part of the Full-Stack-Engineer path and the goal of is:
 - Use command line
 - Develop locally on your computer
 - Document the API using Swagger
+
 ## Technologies
+
 Project is created with:
-##### Server
-- express 4.18.1 
-- express-async-errors 3.1.1 
-- express-flash 0.0.2 
-- express-rate-limit 6.4.0 
-- express-session 1.17.3 
-- passport 0.6.0 
-- passport-local 1.0.0 
-- bcrypt 5.0.1 
-- body-parser 1.20.0 
-- dotenv 16.0.1 
-- cors 2.8.5 
-- helmet 5.1.0 
+
+### Server
+
+- express 4.18.1
+- express-async-errors 3.1.1
+- express-flash 0.0.2
+- express-rate-limit 6.4.0
+- express-session 1.17.3
+- passport 0.6.0
+- passport-local 1.0.0
+- bcrypt 5.0.1
+- body-parser 1.20.0
+- dotenv 16.0.1
+- cors 2.8.5
+- helmet 5.1.0
 - xss-clean 0.1.1
-- memorystore 1.6.7 
-- morgan 1.10.0 
-- path 0.12.7 
-- serve-favicon 2.5.0 
-##### Database
+- memorystore 1.6.7
+- morgan 1.10.0
+- path 0.12.7
+- serve-favicon 2.5.0
+
+### Database
+
 - PostgreSQL v14.2
-- pg 8.7.3 
-- pg-promise 10.11.1 
-##### Documentation
-- swagger-jsdoc 6.2.1 
-- swagger-ui-express 4.4.0 
+- pg 8.7.3
+- pg-promise 10.11.1
+
+### Documentation
+
+- swagger-jsdoc 6.2.1
+- swagger-ui-express 4.4.0
+
 ## Features
-#### Shopping
+
+### Shopping
+
 Shopping routes that allow shoppers to browse by category or search for products
 Persistent carts that consolidate when user logs-in/registers so shopping data is not lost
+
 #### User Accounts
+
 Users can create an account to save shopping session and view information about their orders
+
 #### Checkout Flow
+
 Once a user finish shopping a checkout function summarizes, applies discounts add taxes to the cart and create an order and a account registry
+
 #### Security
+
 Custom hashing function for passwords using bcrypt
 Custom RSA authentication middleware using secure passport and local strategy  to protect against CSRF
 Custom data sanitizer and validation for protection against XSS attacks
+
 #### API Documentation
+
 Documentation with Swagger UI
 Can try out endpoints with test data via Swagger UI, connected to a test database
 Parameters, request body, and response options are documented for each endpoint
 Can create an account and authorize to access all endpoints via Swagger UI
+
 ## Setup
+
 To run locally, first install node_modules and generate RSA Key Pair:
-```
-npm install     
+
+```.
+npm install
 ```
 
 Open a PostgreSQL database of your choice. Schema with tables is located in db/init.sql. E.g., generate tables by running:
+
+```.
+npm run create-db 
 ```
-npm run create-db
-```
-#### Environment Variables
+
+### Environment Variables
+
 Create a .env file in root directory and add the following fields with respective values:
-###### Postgres Database
+
+#### Postgres Database
+
 - PGHOST=
 - PGUSER=
 - PGDATABASE=
 - PGPASSWORD=
 - PGPORT=
-###### Express server
+
+#### Express server
+
 - PORT=
 - SESSION_SECRET=
-###### Node.js 
+
+#### Node.js
+
 - NODE_ENV=
 Then run the app:
 node index.js
+
 ### Configuration / Implementation
-A PostgreSQL database will comprise the data layer of the application. The database's schema will be designed using the  dbdiagram.io(https://dbdiagram.io/d)  tool and the database will be implemented through the  psql(https://www.postgresql.org/docs/current/app-psql.html)  CLI for PostgreSQL.
+
+A PostgreSQL database will comprise the data layer of the application. The database's schema will be designed using the  dbdiagram.io(<https://dbdiagram.io/d>)  tool and the database will be implemented through the  psql(<https://www.postgresql.org/docs/current/app-psql.html>)  CLI for PostgreSQL.
 The application's server layer will be implemented on  
 Node.js -> environment
-Express.js -> server and routing functionality 
+Express.js -> server and routing functionality
 Passport.js -> authentication and authorization
 pg-promises ->database integration
 express-session -> session management
 Finally,  GitHub is being used as the project's version control system.
+
 ### Database Schema
-![resources/ERD%20ecomm-fjor.png](https://github.com/fjorwb/ecommerce-rest-api-project/blob/master/resources/ERD%20ecomm-fjor.png)
+
+![schema](https://github.com/fjorwb/ecommerce-rest-api-project/blob/main/resources/ERD%20ecomm-fjor.png)
+
 ## Usage
+
 This project can be used as a backend for an e-commerce website. The project handles various endpoints a user may need to access while online shopping such as:
+
 - creating user accounts
 users can save first and last name, email, and password to register and shopping
 - displaying products and allowing query by parameter
@@ -122,10 +169,15 @@ users can save first and last name, email, and password to register and shopping
 - checkout flow and charging payments
 - order summaries accessed through user account
 - Authentication and Authorization with three roles: admin, manager, and user
+
 ## Project Status
+
 IN PROGRESS: Working on additional security measures
+
 ## Room for Improvement
+
 Room for improvement:
+
 - Encryption of data in database
 - Add more indexes to the database for faster queries
 To do:
@@ -133,7 +185,11 @@ To do:
 - Send confirmation email after POSTing order
 - Build demo frontend site
 - Incorporate Stripe payment gateway
+
 ## Acknowledgements
+
 This project was based on Codecademy's full-stack portfolio project in the Full-Stack Engineer Career Path.
+
 ## License
+
 ## Contact
--- src/model/setupDatabase.js
+++ src/model/setupDatabase.js
@@ -1,84 +1,99 @@
 require('dotenv').config()
+
 const { DB } = require('../../config')
 
 const { Client } = require('pg')
 
 const database = async () => {
-  const createDatabase = 'CREATE DATABASE IF NOT EXISTS ecomm_nep'
+  const createDatabase = 'CREATE DATABASE ecomm_nep'
 
   const usersTableStatement = `
-        CREATE TABLE IF NOT EXISTS users(
-            id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            user_id                 VARCHAR(6)      NOT NULL,
-            name                    VARCHAR(50)     NOT NULL,
-            email                   VARCHAR(50)     NOT NULL,
-            password                VARCHAR(100)    NOT NULL,
-            role                    VARCHAR(10)    NOT NULL
-        )
-    `
+    CREATE TABLE users(
+      id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      user_id                 VARCHAR(6)      NOT NULL,
+      name                    VARCHAR(50)     NOT NULL,
+      email                   VARCHAR(50)     NOT NULL,
+      password                VARCHAR(100)    NOT NULL,
+      role                    VARCHAR(10)    NOT NULL
+    )
+  `
 
   const categoriesTableStatement = `
-        CREATE TABLE IF NOT EXISTS categories(
-            id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            category_id             VARCHAR(4)      NOT NULL,
-            category_name           VARCHAR(20)     NOT NULL,
-            category_description    VARCHAR(50)     NOT NULL
-        )
-    `
+    CREATE TABLE categories(
+      id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      category_id             VARCHAR(4)      NOT NULL,
+      category_name           VARCHAR(20)     NOT NULL,
+      category_description    VARCHAR(50)     NOT NULL
+    )
+  `
 
   const productsTableStatement = `
-        CREATE TABLE IF NOT EXISTS products (
-            id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            product_id              VARCHAR(9)      NOT NULL,
-            name                    VARCHAR(20)     NOT NULL,
-            description             VARCHAR(50)     NOT NULL,
-            unit                    VARCHAR(10)     NOT NULL,
-            price                   NUMERIC(10,2)   NOT NULL,
-            discount                NUMERIC(6,3)    NOT NULL,
-            img                     VARCHAR(100)    NOT NULL
-        )
-    `
+    CREATE TABLE products (
+      id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      product_id              VARCHAR(9)      NOT NULL,
+      name                    VARCHAR(20)     NOT NULL,
+      description             VARCHAR(50)     NOT NULL,
+      category_id             VARCHAR(4)      NOT NULL,
+      unit                    VARCHAR(10)     NOT NULL,
+      price                   INT             NOT NULL,
+      discount                INT             NOT NULL,
+      img                     VARCHAR(100)    NOT NULL
+    )
+  `
 
   const cartTableStatement = `
-        CREATE TABLE IF NOT EXISTS cart (
-            id                      BIGINT          PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            cart_id                 VARCHAR(6)      NOT NULL,
-            user_id                 VARCHAR(6)      NOT NULL,
-            product_id              VARCHAR(9)      NOT NULL,
-            quantity                INT             NOT NULL,
-            date                    BIGINT          NOT NULL
-        )
-    `
+    CREATE TABLE cart (
+      id                      BIGINT          PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      cart_id                 VARCHAR(6)      NOT NULL,
+      user_id                 VARCHAR(6)      NOT NULL,
+      product_id              VARCHAR(9)      NOT NULL,
+      quantity                INT             NOT NULL,
+      price                   INT             NOT NULL,
+      discount                INT             NOT NULL,
+      date                    BIGINT          NOT NULL
+    )
+  `
 
   const ordersTableStatement = `
-        CREATE TABLE IF NOT EXISTS orders (
-            id                      BIGINT          PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            order_id                VARCHAR(6)      NOT NULL,
-            cart_id                 VARCHAR(6)      NOT NULL,
-            user_id                 VARCHAR(6)      NOT NULL,
-            account_id              VARCHAR(6)      NOT NULL,
-            product_id              VARCHAR(9)      NOT NULL,
-            quantity                INT             NOT NULL,
-            price                   NUMERIC(10,2)   NOT NULL,
-            discount                NUMERIC(6,3)    NOT NULL,
-            tax                     NUMERIC(6,3)    NOT NULL,
-            date                    BIGINT          NOT NULL
-        )
-    `
+    CREATE TABLE orders (
+      id                      BIGINT          PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      order_id                VARCHAR(6)      NOT NULL,
+      cart_id                 VARCHAR(6)      NOT NULL,
+      user_id                 VARCHAR(6)      NOT NULL,
+      account_id              VARCHAR(6)      NOT NULL,
+      product_id              VARCHAR(9)      NOT NULL,
+      quantity                INT             NOT NULL,
+      price                   INT             NOT NULL,
+      discount                INT             NOT NULL,
+      tax                     INT             NOT NULL,
+      date                    BIGINT          NOT NULL
+    )
+  `
 
   const accountsTableStatement = `
-        CREATE TABLE IF NOT EXISTS accounts (
-            id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-            account_id              VARCHAR(10)     NOT NULL,
-            user_id                 VARCHAR(6)      NOT NULL,
-            order_id                VARCHAR(6)      NOT NULL,
-            amount                  NUMERIC(10,2)   NOT NULL,
-            accotype                VARCHAR(2)      NOT NULL, 
-        )
-    `
+    CREATE TABLE accounts (
+      id                      INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      account_id              VARCHAR(10)     NOT NULL,
+      user_id                 VARCHAR(6)      NOT NULL,
+      order_id                VARCHAR(6)      NOT NULL,
+      amount                  INT             NOT NULL,
+      tax                     INT             NOT NULL,
+      accotype                VARCHAR(2)      NOT NULL,
+      date                    BIGINT          NOT NULL 
+    )
+  `
 
+  const sessionTableStatement = `
+    CREATE TABLE session (
+      sid                    VARCHAR          PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      sess                   JSON             NOT NULL,
+      expire                 TIMESTAMP WITHOUT TIME ZONE NOT NULL
+    )
+  `
+
+  // Connect to heroku
   try {
-    const bd = new Client({
+    const db = new Client({
       user: DB.PGUSER,
       host: DB.PGHOST,
       database: DB.PGDATABASE,
@@ -86,18 +101,19 @@ const database = async () => {
       port: DB.PORT
     })
 
-    await bd.connect()
+    await db.connect()
 
     // Create tables on database
-    await bd.query(createDatabase)
-    await bd.query(usersTableStatement)
-    await bd.query(categoriesTableStatement)
-    await bd.query(productsTableStatement)
-    await bd.query(cartTableStatement)
-    await bd.query(ordersTableStatement)
-    await bd.query(accountsTableStatement)
+    await db.query(createDatabase)
+    await db.query(usersTableStatement)
+    await db.query(categoriesTableStatement)
+    await db.query(productsTableStatement)
+    await db.query(cartTableStatement)
+    await db.query(ordersTableStatement)
+    await db.query(accountsTableStatement)
+    await db.query(sessionTableStatement)
 
-    await bd.end()
+    await db.end()
   } catch (error) {
     console.log('Error creating one or more tables: ', error)
   }
